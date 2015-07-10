aa8a0e8fc8be191c5da284e2da1a7a6b1d6b3f35
yoshida10 <yoshidatomonari9@gmail.com>
2015-07-10 12:06:07 +0900

    ローカルなネットワーク通信の実装

---
+++ Assets/02.Scripts/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+
+	public Behaviour cntrlScript;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer) {
+			cntrlScript.enabled=false;
+			camera.enabled=false;
+		}
+	}
+
+	void OnApplicationFocus(bool focusStatus)
+	{
+		if(isLocalPlayer)
+		{
+			cntrlScript.enabled=focusStatus;
+			camera.enabled=true;
+		}
+	}
+}
